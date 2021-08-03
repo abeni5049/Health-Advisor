48f19c3750bb0473e160f73e34d78289988c3c05
abeni <abeni5049@gmail.com>
2021-08-03 17:45:51 +0300

    remove view model from bottom nav bar

--- app/src/main/java/com/example/healthadvisor/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/healthadvisor/ui/dashboard/DashboardFragment.java
@@ -17,24 +17,15 @@ import com.example.healthadvisor.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
--- app/src/main/java/com/example/healthadvisor/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/healthadvisor/ui/notifications/NotificationsFragment.java
@@ -17,24 +17,16 @@ import com.example.healthadvisor.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 
