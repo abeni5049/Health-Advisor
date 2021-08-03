c1388b469afc332cb15290c418b79a4ac7f4eb1a
abeni <abeni5049@gmail.com>
2021-08-03 17:43:18 +0300

    add mother page with custom nav bar

--- app/src/main/java/com/example/healthadvisor/LoginActivity.java
+++ app/src/main/java/com/example/healthadvisor/LoginActivity.java
@@ -7,21 +7,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 //TODO remove dark mode.
+    private int userType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Log IN");
 
         Spinner spinner = (Spinner) findViewById(R.id.usertype_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.usertype_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        Button loginButton = findViewById(R.id.login_button);
+        userType = 0;
+        loginButton.setOnClickListener(v -> {
+            if (userType == 0) {
+                Intent intent = new Intent(LoginActivity.this, MotherActivity.class);
+                startActivity(intent);
+//                case 1:
+//                    Intent intent = new Intent(LoginActivity.this,FpWorkerActivity.class);
+//                      startActivity(intent);
+//                case 2:
+//                    Intent intent = new Intent(LoginActivity.this,physician.class);
+//                startActivity(intent);
+//                case 3:
+//                    Intent intent = new Intent(LoginActivity.this,Admin.class);
+//                    startActivity(intent);
+            }else {
+                Toast.makeText(this, "error occurred", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         TextView register = findViewById(R.id.register);
         register.setOnClickListener(v -> {
@@ -32,12 +57,11 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnIt
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
+        userType = position;
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+
     }
 }
