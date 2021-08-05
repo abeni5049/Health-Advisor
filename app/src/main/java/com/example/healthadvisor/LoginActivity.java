7c8b31799955e94c22eafd4da24de6e70842486e
eyouale <teyouale@gmail.com>
2021-08-05 17:30:51 +0300

    Adding Firebase Firestore

--- app/src/main/java/com/example/healthadvisor/LoginActivity.java
+++ app/src/main/java/com/example/healthadvisor/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.healthadvisor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +14,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -31,6 +40,31 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnIt
         Button loginButton = findViewById(R.id.login_button);
         loginButton.setOnClickListener(v -> {
             userType = spinner.getSelectedItemPosition();
+//            /**/
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            // Create a new user with a first and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("first", "Ada");
+            user.put("last", "Lovelace");
+            user.put("born", 1815);
+
+// Add a new document with a generated ID
+            db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error adding document", e);
+                        }
+                    });
+
             Intent intent;
             switch(userType) {
                 case 0:
